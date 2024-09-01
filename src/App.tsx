4504abca173eeb1a0228ca649a29e29e93895c38
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-09-01 14:14:55 +0200

    Dooone

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="Guessed">
+       <p>Wow godt gættet</p>
+       <p>Det tog: 0 forsøg</p>
       </header>
     </div>
   );
---
+++ src/GuessInput.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+
+export default function GuessInput(){
+
+    const [guess, setGuess] = useState<number>(0);
+    const [inputValue, setInputValue] = useState<number | undefined>(undefined);
+    const [lastGuess, setLastGuess] = useState<number>(0)
+    const [lowest, setLowest] = useState<number>(0)
+    const [highest, setHighest] = useState<number>(100)
+
+    const [randomNumber, setRandomNumber] = useState(0)
+    const [attempts, setAttempts] = useState(0)
+    const [answer, setAnswer] = useState<JSX.Element[]>([])
+
+    // 0 = equal, 1 = higher, -1 = lower
+    const [algoDirection, setAlgoDirection] = useState<number>(0)
+    useEffect(() => {
+        setRandomNumber(Math.floor( Math.random()*100))
+        setAnswer([<p>Guess a number between 0 and 100</p>])
+    }, []);
+
+    const handleGuessChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(Number(event.target.value));
+    };
+    function addToAnswer(message: string){
+        setAnswer([...answer, <p>{message}</p>])
+    }
+    function compare(number: number){
+        return number - randomNumber
+    }
+    function getRandomInt(x: number, y: number){ 
+    if (x > y) [x, y] = [y, x];
+
+    return Math.floor(Math.random() * (y - x + 1)) + x;
+    }
+    function giveGuess() {
+        if (guess === undefined) return;
+    
+        setAttempts(attempts + 1);
+        const comp = compare(guess);
+        if (comp === 0) {
+            addToAnswer("Tillykke! Du gættede rigtigt!, Det tog dig " + attempts + " forsøg");
+        } else {
+            if (comp < 0) {
+                addToAnswer("Højere end " + guess);
+                setAlgoDirection(1);
+                setLowest(guess);
+            } else {
+                addToAnswer("Lavere end " + guess);
+                setAlgoDirection(-1);
+                setHighest(guess);
+            }
+        }
+        setLastGuess(guess);
+
+        console.log("lowest", lowest);
+        console.log("highest", highest);
+        console.log("lastGuess", lastGuess);
+        console.log("direction", algoDirection);
+        console.log("guess", guess);
+    }
+    useEffect(() => {
+        if (guess !== undefined && guess !== 0) {
+            giveGuess();
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [guess]);
+
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (inputValue !== undefined) {
+            setGuess(inputValue);
+        }
+    };
+
+
+
+    const giveGuessHandler = () => {
+        let newGuess: number;
+
+        // give a random guess between last guess
+        if (lastGuess === 0) {
+        newGuess = getRandomInt(0, 100);
+        } else if (algoDirection === 1) {
+            newGuess = getRandomInt(lastGuess, highest -1);
+        } else if (algoDirection === -1) {
+            newGuess = getRandomInt(lowest +1, lastGuess);
+        } else {
+            newGuess = getRandomInt(0, 100);
+        }
+        
+        setGuess(newGuess);
+    }
+
+    return (
+        <>
+        <p>{answer}</p>
+        <form onSubmit={handleSubmit}>
+            <input name="guess" value={inputValue} placeholder="" type="number" onChange={handleGuessChange}></input>
+            <button type="submit">Gæt</button>
+        </form>
+        <button onClick={giveGuessHandler} >Guess for me!</button>
+        </>
+    );
+
+
+
+}
